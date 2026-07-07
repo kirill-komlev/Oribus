d6efabf1ab2f12eae4e904a5adad090a3103b798
Kirill Komlev <kirillkomlev02@gmail.com>
2026-07-07 23:49:57 +0300

    Сверстал Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { Calendar, ChartLine, House, ListChecks, LogOut, Settings, Sparkles, Users } from 'lucide-react'
+
+import Logo from '/icons/logo.png'
+
+import { PAGE_CONFIG } from '../../configs/page-config'
+import { NavbarLink } from './NavbarLink'
+
+export function Navbar() {
+	return (
+		<nav className='px-6 py-4 flex flex-col gap-4 items-center border-r-2 border-gray-200'>
+			<img
+				src={Logo}
+				alt='Oripio Design'
+				className='w-12 h-12'
+			/>
+			<hr className='w-full border border-gray-100' />
+			<div className='flex flex-col gap-4 items-center'>
+				<NavbarLink to={PAGE_CONFIG.home}>
+					<House strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<ListChecks strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<Users strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<Calendar strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<ChartLine strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<Sparkles strokeWidth={1.5} />
+				</NavbarLink>
+			</div>
+
+			<div className='mt-auto flex flex-col gap-4 items-center'>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<Settings strokeWidth={1.5} />
+				</NavbarLink>
+				<NavbarLink to={PAGE_CONFIG.none}>
+					<LogOut strokeWidth={1.5} />
+				</NavbarLink>
+			</div>
+		</nav>
+	)
+}
---
+++ src/components/navbar/NavbarLink.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router'
+import { cn } from '../../libs/cn/cn'
+
+const baseStyles = 'p-3 rounded-xl transition-all duration-200 outline-offset-2 outline-transparent hover:outline-gray-300 outline-2'
+
+export function NavbarLink({ to, children, ...props }) {
+	return (
+		<NavLink
+			to={to}
+			className={({ isActive }) => (isActive ? cn('bg-gray-900 text-white', baseStyles) : baseStyles)}
+			{...props}
+		>
+			{children}
+		</NavLink>
+	)
+}
