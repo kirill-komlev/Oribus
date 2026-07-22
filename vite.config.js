48e783d8a440ac836f65ea9cf0e9a5683887c99c
Kirill Komlev <kirillkomlev02@gmail.com>
2026-07-23 01:18:54 +0300

    Изменил базовые конфиги проекта

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			src: '/src',
+			app: '/src/app',
+			pages: '/src/pages',
+			components: '/src/components',
+			configs: '/src/configs',
+			contexts: '/src/contexts',
+			hooks: '/src/hooks',
+			layouts: '/src/layouts',
+			libs: '/src/libs',
+			shared: '/src/shared',
+			stores: '/src/stores',
+		},
+	},
 })
