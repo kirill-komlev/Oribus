a3c7fa6c0aafb1f4cf81270e501a85abd62a6378
Kirill Komlev <kirillkomlev02@gmail.com>
2026-08-01 23:28:00 +0300

    Обернул App в QueryClientProvider

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,15 @@
 import { Router } from './router/Router'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</QueryClientProvider>
 	)
 }
